2a965f93395df9adac15ae48e0b16b2286ea00ce
Amen Souissi <amensouissi@ecreall.com>
2017-09-18 12:06:37 +0200

    fix and cleanup

--- hdm/__init__.py
+++ hdm/__init__.py
@@ -7,5 +7,6 @@ def main(global_config, **settings):
     """
     config = Configurator(settings=settings, root_factory=root_factory)
     config.include('substanced')
+    config.include('dace')
     config.scan()
     return config.make_wsgi_app()
