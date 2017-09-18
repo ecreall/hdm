131212c2e16755aad323cc64860a00ac7ec503bb
Amen Souissi <amensouissi@ecreall.com>
2017-09-18 13:00:39 +0200

    Example with ui

--- hdm/process_definition.py
+++ hdm/process_definition.py
@@ -16,7 +16,7 @@ from dace.util import get_all_business_actions
 # Step 1: Define a behavior to execute. This behavior is an
 # 'ElementaryAction'. It means that the behavior is executed
 # only one time in the process instance.
-class MyBehavior(ElementaryAction):
+class MyHelloBehavior(ElementaryAction):
     context = Interface
 
     def start(self, context, request, appstruct, **kw):
@@ -24,12 +24,20 @@ class MyBehavior(ElementaryAction):
         return {'message': 'Hello world!'}
 
 
+class MyByBehavior(ElementaryAction):
+    context = Interface
+
+    def start(self, context, request, appstruct, **kw):
+        # Your code hear
+        return {'message': 'Bye world!'}
+
 # Step 2: Define the process with her nodes and transitions
 @process_definition(
     id='myprocessid',
     title='My process')
 class MyProcess(ProcessDefinition):
-
+    is_unique = True
+    is_volatile = True
     def init_definition(self):
         # define process nodes
         self.define_nodes(
@@ -37,25 +45,33 @@ class MyProcess(ProcessDefinition):
             start=StartEventDefinition(),
             # hello node
             hello=ActivityDefinition(
-                # MyBehavior is the behavior to execute
+                # MyHelloBehavior is the behavior to execute
                 # when the node is called
-                behaviors=[MyBehavior],
+                behaviors=[MyHelloBehavior],
                 description='Hello behavior',
                 title='Hello!'),
+            # hello node
+            bye=ActivityDefinition(
+                # MyByBehavior is the behavior to execute
+                # when the node is called
+                behaviors=[MyByBehavior],
+                description='Bye behavior',
+                title='Bye!'),
             # end node: the ending of the process
             end=EndEventDefinition(),
         )
         # define transitions between process nodes
         self.define_transitions(
             TransitionDefinition('start', 'hello'),
-            TransitionDefinition('hello', 'end'),
+            TransitionDefinition('hello', 'bye'),
+            TransitionDefinition('bye', 'end'),
         )
 
 
 # Step 3: Define a simple view for find, execute and
 # display the result of the execution of our behavior
 # defined in our process.
-@view_config(name='my_process', renderer='json')
+@view_config(name='my_process', renderer='hdm:templates/my_process.pt',)
 def my_process_view(request):
     # 'get_all_business_actions' enable to retrieve all of
     # behaviors in all of process instances with the id
@@ -76,9 +92,9 @@ def my_process_view(request):
         # See 'start' method of MyBehavior class
         excution = result.get('message', None)
         # Get the process instance id
-        process_id = action_to_execute.process.__name__
-        return {'action title': action_title,
+        process_id = getattr(action_to_execute.process, '__name__', '')
+        return {'action_title': action_title,
                 'message': excution,
-                'process id': process_id}
+                'process_id': process_id}
 
     return {}
