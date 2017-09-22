d223a0aabdc8b57c2dc92985da379b872d512fa6
Guillaume Troupel <guillaume.troupel@gmail.com>
2017-09-22 10:08:21 +0200

    fix endpoint name

--- hdm/views/hdm_process/pending_vacation_requests.py
+++ hdm/views/hdm_process/pending_vacation_requests.py
@@ -1,7 +1,7 @@
 from pyramid.view import view_config
 
 
-@view_config(name='pendent_vacation_requests', renderer='hdm:views/hdm_process/templates/vacations.pt', )
+@view_config(name='pending_vacation_requests', renderer='hdm:views/hdm_process/templates/vacations.pt', )
 def pending_vacations_view(request):
     """ A view to list all pending vacations """
     pending_vacations = [vacation for vacation in request.root.vacations if 'pending' in vacation.state]
